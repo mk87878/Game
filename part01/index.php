86315c397512717d3b4b0834e8520f3175dee601
mk87878 <mk87878@163.com>
2016-07-13 17:58:44 +0800

    完成注册页面前端

--- part01/index.php
+++ part01/index.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <title>注册与登录</title>
+    <style>
+        p span{
+            color: #ee281d;
+            font-size: 62.5%;
+            padding-left: 10px;
+        }
+    </style>
 </head>
+
+<?php
+include_once 'config/config.php';
+
+?>
+
 <body>
-<h3>注册</h3>
-<p>用户名: <input type="text"><span></span></p>
+<form action="" method="post">
+    <h3>注册</h3>
+    <p>用户名: <input type="text" name="userName" placeholder=""><span>仅支持邮箱作为用户名</span></p>
+    <p>姓名: <input type="text" name="name" placeholder=""><span>仅支持字母</span></p>
+    <p>手机: <input type="text" name="tel" placeholder=""><span>11位数字</span></p>
+    <p>密码: <input type="text" name="passWord" placeholder=""><span>8个或以上,不重复字符数字和字母</span></p>
+    <p><button>注册</button></p>
+</form>
 </body>
 </html>
