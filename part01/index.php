2c645c209518d8cb5ebc22205f466c88135b334e
mk87878 <mk87878@163.com>
2016-07-13 17:18:11 +0800

    建立首页登录页面

---
+++ part01/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>注册与登录</title>
+</head>
+<body>
+<h3>注册</h3>
+<p>用户名: <input type="text"><span></span></p>
+</body>
+</html>
