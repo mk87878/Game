b708ff2815bf310351a3be69ee638b91c3a55e5f
mk87878 <mk87878@163.com>
2016-07-19 15:10:03 +0800

    增加自学测试文件夹other

---
+++ other/Upload/upload.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>文件上传</title>
+</head>
+
+<?php
+/**
+ * Author: http://hizip.net
+ * Date: 16/7/19
+ * Time: 15:09
+ */
+?>
+<body>
+
+</body>
+</html>
